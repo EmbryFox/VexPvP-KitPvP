0cadabb5c66734f54b09f0cabac180054a567c70
$y <85597036+xyz-lang@users.noreply.github.com>
2022-07-15 01:00:30 -0600

    Update ScoreboardScoresGetter.java

--- src/main/java/me/steveshat/vexkits/scoreboard/ScoreboardScoresGetter.java
+++ src/main/java/me/steveshat/vexkits/scoreboard/ScoreboardScoresGetter.java
@@ -12,12 +12,12 @@ import java.util.LinkedList;
 public class ScoreboardScoresGetter implements ScoreGetter {
     @Override
     public void getScores(@NotNull LinkedList<String> linkedList, @NotNull Player player) {
-        String rank = PlaceholderAPI.setPlaceholders(player, ".skript_rank.");
-        String balance = PlaceholderAPI.setPlaceholders(player, ".skript_balance.");
-        String kills = PlaceholderAPI.setPlaceholders(player, ".skript_kills.");
-        String deaths = PlaceholderAPI.setPlaceholders(player, ".skript_kills.");
-        String kdr = PlaceholderAPI.setPlaceholders(player, ".skript_kdr.");
-        String streak = PlaceholderAPI.setPlaceholders(player, ".skript_streak.");
+        String rank = PlaceholderAPI.setPlaceholders(player, "%skript_rank%");
+        String balance = PlaceholderAPI.setPlaceholders(player, "%skript_balance%");
+        String kills = PlaceholderAPI.setPlaceholders(player, "%skript_kills%");
+        String deaths = PlaceholderAPI.setPlaceholders(player, "%skript_kills%");
+        String kdr = PlaceholderAPI.setPlaceholders(player, "%skript_kdr%");
+        String streak = PlaceholderAPI.setPlaceholders(player, "%skript_streak%");
 
         linkedList.add(Chat.format("&e"));
         linkedList.add(Chat.format("&7 | &dRank: " + rank));
