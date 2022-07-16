78f53e5779ca35a2e85d235daee4bd63c4038ec2
$y <85597036+xyz-lang@users.noreply.github.com>
2022-07-15 19:59:50 -0600

    Update VexKits.java

--- src/main/java/me/steveshat/vexkits/VexKits.java
+++ src/main/java/me/steveshat/vexkits/VexKits.java
@@ -5,10 +5,7 @@ import me.steveshat.vexkits.scoreboard.ScoreboardScoresGetter;
 import me.steveshat.vexkits.scoreboard.ScoreboardTitleGetter;
 import net.evilblock.cubed.scoreboard.ScoreboardHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,25 +23,5 @@ public final class VexKits extends JavaPlugin {
         ScoreboardHandler.configure(new ScoreboardTitleGetter(), new ScoreboardScoresGetter());
 
 
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (command.getName().equalsIgnoreCase("vex")) {
-
-            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-
-                if (sender.hasPermission("vex.reload")) {
-
-                    Player p = (Player) sender;
-                    plugin.reloadConfig();
-                    p.sendMessage(ChatColor.LIGHT_PURPLE + "Reloaded vex config");
-
-                }
-            }
-        }
-
-        return true;
     }
 }
